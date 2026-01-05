110cec57e9f2b45c847e22a1523db175b3ba386f
Marshiayce <abriomaiii@gmail.com>
2026-01-05 18:30:33 +0800

    added grades

--- lab2_student_record.py
+++ lab2_student_record.py
@@ -1,7 +1,7 @@
 student = {
     "id" : "2025-001",
     "name" : "Juan Dela Cruz",
-    "grades" : [80,90,85]
+    "grades" : [80,90,85,72,76]
 }
 
 average = sum(student["grades"])/len(student["grades"])
