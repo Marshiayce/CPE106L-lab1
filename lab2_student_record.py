987257feb1b4f1bdff45296170b4cc5b0b490baa
Marshiayce <abriomaiii@gmail.com>
2026-01-05 18:29:10 +0800

    lab 2 activity

---
+++ lab2_student_record.py
@@ -0,0 +1,8 @@
+student = {
+    "id" : "2025-001",
+    "name" : "Juan Dela Cruz",
+    "grades" : [80,90,85]
+}
+
+average = sum(student["grades"])/len(student["grades"])
+print("Average:", average)
